dd7377d86df33ca09764aa332c45da82c4998342
Jaeho Jeong <ywm2004@gmail.com>
2021-03-08 14:33:42 +0900

    # [ LogIn alter 기능 추가 ]

--- BlackUp_Clone_Project/src/servlet/LoginServlet.java
+++ BlackUp_Clone_Project/src/servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -28,10 +29,11 @@ public class LoginServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
 
 		// DataSource를 가져오기 위해 ServletContext 변수 생성
 		ServletContext sc = req.getServletContext();
-        
+
 		// req.getAttribute로 사용자의 입력값 받아오기.
 		// UserDAO를 이용하여 로그인 검사
 		UserDAO userDAO = (UserDAO) sc.getAttribute("userDAO");
@@ -39,27 +41,42 @@ public class LoginServlet extends HttpServlet {
 		UserDTO userDTO = new UserDTO().setUserID(req.getParameter("userID"))
 				.setUserPassword(req.getParameter("userPassword"));
 
-	
+		resp.setContentType("text/html; charset=utf-8");
 		int result = userDAO.login(req.getParameter("userID"), req.getParameter("userPassword"));
 
 		if (result == 1) { // 로그인 성공 시
 			System.out.println("로그인이 성공하였습니다.");
 			resp.sendRedirect("main");
-			
-		} else if (result == 0) { // 비밀번호가 틀렸을 경우
-			System.out.println("비밀번호가 틀렸습니다.");
-			resp.sendRedirect("login");
-		} else if (result == -1) { // 아이디가 존재하지 않은 경우
-			System.out.println("존재하지 않은 아이디입니다.");
-			resp.sendRedirect("login");
-		} else if (result == -2) { // DB 오류
-			System.out.println("데이터베이스 오류가 발생했습니다.");
-			resp.sendRedirect("login");
-		} else {
-			System.out.println("오류.");
-			resp.sendRedirect("login");
-		}
 
+		} else if (result == 0) { // 비밀번호가 틀렸을 경우
+			PrintWriter wr = resp.getWriter();
+			wr.println("<script>");
+			wr.println("alert('비밀번호가 틀렸습니다.');");
+			wr.println("location.href = 'login'");
+			wr.println("</script>");
+			wr.close();
+		} else if (result == -1) { // 아이디가 존재하지 않은 경우
+			PrintWriter wr = resp.getWriter();
+			wr.println("<script>");
+			wr.println("alert('존재하지 않은 아이디입니다.');");
+			wr.println("location.href = 'login'");
+			wr.println("</script>");
+			wr.close();
+		} else if (result == -2) { // DB 오류
+			PrintWriter wr = resp.getWriter();
+			wr.println("<script>");
+			wr.println("alert('데이터베이스 오류가 발생했습니다.');");
+			wr.println("location.href = 'login'");
+			wr.println("</script>");
+			wr.close();
+		} else {
+			PrintWriter wr = resp.getWriter();
+			wr.println("<script>");
+			wr.println("alert('오류.');");
+			wr.println("location.href = 'login'");
+			wr.println("</script>");
+			wr.close();
+		}
 
 	}
 }
